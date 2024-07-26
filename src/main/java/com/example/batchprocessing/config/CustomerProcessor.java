1b3c92952563a792fdce62f8ec48215128aba918
aarti-paan <aartipaan@gwmail.gwu.edu>
2024-07-26 16:25:11 -0400

    initial commit

---
+++ src/main/java/com/example/batchprocessing/config/CustomerProcessor.java
@@ -0,0 +1,11 @@
+package com.example.batchprocessing.config;
+
+import com.example.batchprocessing.entity.Customer;
+import org.springframework.batch.item.ItemProcessor;
+
+public class CustomerProcessor implements ItemProcessor<Customer,Customer> {
+    @Override
+    public Customer process(Customer customer) throws Exception{
+        return customer;
+    }
+}
---
+++ src/main/java/com/example/batchprocessing/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.example.batchprocessing.repository;
+
+import com.example.batchprocessing.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer,Integer> {
+
+}
